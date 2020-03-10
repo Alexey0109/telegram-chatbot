06a40370187f3dfdd934f2709c0f91b7d7c2ff2c
Alexey0109 <grudko2003@mail.ru>
2020-03-10 08:52:11 +0300

    commit

--- main.py
+++ main.py
@@ -1,65 +1,34 @@
 import telebot
 
-token = "959976424:AAHQ7FxDWp9kCiRJjCwoj4EOnMQTwDshOsU"
+#TOKEN = '959976424:AAHQ7FxDWp9kCiRJjCwoj4EOnMQTwDshOsU'
+TOKEN = '966351011:AAGDUmgrpOfujpT5flyRlOn26Li-_U8f7Dg'
+MESSAGES = {}
+TEXT = None
+bot = telebot.TeleBot(TOKEN)
 
-bot = telebot.TeleBot(token)
-
-
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    print('Starting!')
-    bot.send_message(message.chat.id, 'Hello! I am a chatbot and you can teach me some new words!')
-@bot.message_handler(commands=['stop'])
-def stop(message):
-    print('Stopping!')
-    bot.send_message(message.chat.id,"Bye :D")
-    exit(0)
-@bot.message_handler(commands=['help'])
-def help(message):
-    bot.send_message(message.chat.id,"Hello! I am a chatbot and you can teach me some new words!\nHere my commands::\n/add_message - add message pattern to DB\n\nIf I know rude words - text @KeyboardDestroyer")
-    
-@bot.message_handler(commands=['add_message'])
+@bot.message_handler(commands=['add'])
 def add_message(message):
-    print('Adding message')
-    bot.send_message(message.chat.id, "Send message text:  ")
-    bot.register_next_step_handler(message, register_message)
-    
-def add_reply(message):
-    bot.send_message(message.chat.id, "Send message text: ")
-    bot.register_next_step_handler(message, register_reply)
-def register_message(message):
-    print(message.text)
-    bot.send_message(message.chat.id, "Send reply text: ")
-    print("Writing message...")
-    msgfile = open('messages.txt', 'a')
-    msgfile.write(str(message.text) + '\n')
-    msgfile.close()
-    bot.register_next_step_handler(message, register_reply)
-def register_reply(message):
-    print(message.text)
-    bot.send_message(message.chat.id, "Writing... ")
-    replyfile = open('replytext.txt', 'a')
-    replyfile.write(str(message.text) + '\n')
-    replyfile.close()
-    
-@bot.message_handler(content_types=['text'])
-def send_text(message):
-    print(message.text, end='; ')
-    msgfile = open('messages.txt', 'r')
-    reply = open('replytext.txt', 'r')
-    num = 0
-    for num, line in enumerate(msgfile, 1):
-        if message.text.lower() in line.lower():
-            reply = open('replytext.txt', 'r')
-            r = ''
-            for j in range(num):
-                r = reply.readline()
-            bot.send_message(message.chat.id, r)
-            print(r)
-            reply.close()
-    msgfile.close()
-    #if message.text.lower() == 'hello':
-    #    bot.send_message(message.chat.id, 'Hello there!')
+    bot.send_message(message.chat.id, 'Enter message: ')
+    bot.register_next_step_handler(message, set_incoming)
+def set_incoming(message):
+    global TEXT
+    text = message.text
+    TEXT = text
+    bot.send_message(message.chat.id, 'Enter reply: ')
+    bot.register_next_step_handler(message, set_reply)
+def set_reply(message):
+    global MESSAGES, TEXT
+    MESSAGES[TEXT] = message.text
+    bot.send_message(message.chat.id, 'Success! ')
 
+@bot.message_handler(content_types=['text'])
+def incoming_message(message):
+    global MESSAGES
+    try:
+        text = MESSAGES[message.text]
+        print(text)
+        bot.send_message(message.chat.id, text)
+    except Exception as e:
+        print(e)
 
 bot.polling()
